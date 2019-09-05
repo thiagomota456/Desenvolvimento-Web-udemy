21940d3bfd0ce9ed3626af40d3a1e4bad89c610c
Thiago Soares Mota <thiagomota456@outlook.com>
2019-09-05 07:41:18 -0300

    Add pra usar do cefet

--- Seção 8 - JavaScript/163 - atividades-para-fixacao-do-conteudo/1.js
+++ Seção 8 - JavaScript/163 - atividades-para-fixacao-do-conteudo/1.js
@@ -1,3 +1,29 @@
-//console.log('Teste')
-var a =0;
-print(a);
+var readline = require('readline');
+var resp = "";
+
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question('Dígite a sua idade: ', function(answer) {
+    var resp = answer;
+
+if(resp<0)
+    console.log('Espirito maligno');
+
+if(resp>=0 && resp<15)
+    console.log('Criança');
+
+if(resp>=15 && resp<30)
+    console.log('Jovem');
+
+if(resp>=30 && resp<60)
+    console.log('Adulto');
+
+if(resp>=60)
+    console.log('Idoso');
+    
+    leitor.close();
+});
+
